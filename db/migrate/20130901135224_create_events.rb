fc1f481b30756d0114d8f79d7511ae1090e75094
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-01 21:50:48 -0500

    add divs and classes for css styling

--- db/migrate/20130901135224_create_events.rb
+++ db/migrate/20130901135224_create_events.rb
@@ -1,6 +1,7 @@
 class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
+      t.string     :creator_name
       t.integer    :user_id
       t.belongs_to :restaurant
       t.datetime   :time
